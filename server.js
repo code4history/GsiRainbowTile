47c9e99d29419569a5545e07f24d034c91b6bee2
Kohei Otsuka <kochizufan@gmail.com>
2022-01-22 18:27:48 +0900

    Upload initial

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.static("build"));
+
+app.listen(app.get("port"), () => {
+  console.log(`http://localhost:${app.get("port")}`);
+});
+
+app.use('/tile', function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+})
