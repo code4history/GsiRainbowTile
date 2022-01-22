7e718b38119e623ae987c41e94ee37c69fe89b77
Kohei Otsuka <kochizufan@gmail.com>
2022-01-22 19:00:23 +0900

    Canvas load test

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
+const {loadImage, createCanvas} = require("canvas");
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-app.use(express.static("build"));
+app.use(express.static('public'));
+
+app.get('/tile', (req, res) => {
+  console.log('Request Type:', req.method)
+  res.send(`Hoge ${loadImage} ${createCanvas}`)
+});
 
 app.listen(app.get("port"), () => {
   console.log(`http://localhost:${app.get("port")}`);
 });
-
-app.use(express.static("public"));
-
-app.get('/tile', function (req, res, next) {
-  console.log('Request Type:', req.method)
-  next()
-})
